03bf12da21da49a51c77e49ae7eaeda133b4cabc
Vinícius Aarão Caldas da Costa <vinyby@gmail.com>
2022-04-20 09:26:16 -0300

    Implementacao da classe stack

---
+++ stack-array.js
@@ -0,0 +1,39 @@
+class Stack {
+    constructor(){
+        this.items = [];
+    }
+
+push(element){
+    this.items.push(element);
+    }
+
+pop(){
+    return this.items.pop(); 
+    }
+
+peek(){
+    return this.items[this.items.length - 1];
+    }
+isEmpty(){
+    return this.items.length === 0;
+    }
+
+size(){
+    return this.items.length;
+    }
+
+clear(){
+    this.items = [];
+    }
+}
+
+const stack = new Stack();
+console.log(stack.isEmpty());
+
+stack.clear();
+console.log(stack.size());
+
+stack.push(8);
+console.log(stack.size());
+
+
